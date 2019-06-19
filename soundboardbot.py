4704cc696b0481f27777175539c3c8828ebad383
Jon Martin <jonnitram@JonNitram.local>
2019-06-19 11:34:31 -0500

    fixed finished error typo

--- soundboardbot.py
+++ soundboardbot.py
@@ -430,11 +430,11 @@ async def finished_command(message):
     finished.remove(command)
     setup_commands()
     if command in audio_commands:
-        result = 'This command has finished being created: '
+        result = 'This command has finished being created: ' + command
     else:
         result = message.author.mention + ' Something went wrong when creating this command: '
-    result += command + '\n'
-    result += 'If this failed instantly, then the video cannot be downloaded. Otherwise your command duration extended < 1 second passed the end of the video.'
+        result += command + '\n'
+        result += 'If this failed instantly, then the video cannot be downloaded. Otherwise your command duration extended < 1 second passed the end of the video.'
     create_new_command_process = None
     downloading.remove(command)
     asyncio.create_task(check_send_message(message, result))
